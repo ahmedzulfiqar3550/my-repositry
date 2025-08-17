8f19cd9b266f942903060510e8a012bc48da542f
ahmedzulfiqar3550 <ahmedzulfiqar3550@gmail.com>
2025-08-17 11:55:48 +0500

    Create class_assignment.py

---
+++ class_assignment.py
@@ -0,0 +1,18 @@
+a = ["full stack"," batch no 4",2025,6 ]
+print(a)
+print(a[1])
+print(a.append("ahmed"))
+print(a.insert(0,"new"))
+print(a.remove("full stack"))
+print(a.pop(2))
+a[1] = 500
+print(a)
+print(type(a))
+print(type(a[3]))
+
+
+for i in a :
+    print(i)
+
+print("end")
+
